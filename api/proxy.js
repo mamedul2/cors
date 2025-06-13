f7d40841fdbd252676bf1dfb5c9637832ef3439e
mamedul2 <md.mamedulislam+2@gmail.com>
2025-06-13 22:31:34 +0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -29,6 +29,17 @@ export default async function handler(req, res) {
     res.status(proxyResponse.status);
     proxyResponse.headers.forEach((value, name) => res.setHeader(name, value));
 
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Handle preflight OPTIONS request
+    if (req.method === 'OPTIONS') {
+      res.status(204).end();
+      return;
+    }
+
     // Stream back the response
     const proxyBody = await proxyResponse.arrayBuffer();
     res.send(Buffer.from(proxyBody));
